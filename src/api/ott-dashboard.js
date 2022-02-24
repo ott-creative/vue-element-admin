8eeab6f1e78b713bf346c38f517beba2ffc1db51
is-shiwen <wen.shi@ott.ca>
2022-02-24 16:14:38 +0800

    修改看板的请求地址

--- src/api/ott-dashboard.js
+++ src/api/ott-dashboard.js
@@ -2,8 +2,7 @@ import request from '@/utils/request'
 
 export function fetchList() {
   return request({
-    // url: 'https://club-service.codegene.xyz/admin/viewData',
-    url: 'http://127.0.0.1:7003/admin/viewData',
+    url: 'https://club-service.codegene.xyz/admin/viewData',
     method: 'get'
   })
 }
